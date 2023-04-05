0cbd1161d685acfd91a5f0536c60cddaff47b554
MiguelsGit <miguelrlopes01@gmail.com>
2023-04-05 11:50:32 +0100

    Changes to the logout system

--- my-app/pages/LogoutGoogle.js
+++ my-app/pages/LogoutGoogle.js
@@ -1,8 +1,39 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import styles from '@/styles/Home.module.css';
+import { onAuthStateChanged } from 'firebase/auth';
 import { authGoogle } from '@/config/firebaseConfig';
 import { signOut } from 'firebase/auth';
 
-const logout = async () => {
-  await signOut(authGoogle);
+const LogoutGoogle = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(authGoogle, (user) => {
+      if (!user) {
+        router.push('/'); // Redirect to the home page when the user logs out
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleLogout = async () => {
+    await signOut(authGoogle);
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Logging Out...</h1>
+      <button className={styles.button} onClick={handleLogout}>
+        Confirm Logout
+      </button>
+      <Link href="/">
+        <button className={styles.button}>Cancel Logout</button>
+      </Link>
+    </div>
+  );
 };
 
-export default logout;
+export default LogoutGoogle;
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,16 +1,12 @@
-// Import necessary modules and components
 import Link from 'next/link'; // Next.js component for client-side navigation
 import styles from '@/styles/Home.module.css'; // CSS styles for this component
 import { onAuthStateChanged } from 'firebase/auth'; // Firebase authentication listener
 import { useState, useEffect } from 'react'; // React hooks for managing component state
-import logout from '../pages/LogoutGoogle'; // Function for logging out of Google account
 import { authGoogle } from '@/config/firebaseConfig'; // Firebase authentication configuration
 
 function HomePage() {
-  // Declare a state variable for the user's authentication status
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null); // Declare a state variable for the user's authentication status
 
-  // Add a Firebase authentication listener when the component mounts
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(authGoogle, (user) => {
       setUser(user); // Set the user state based on the current authentication status
@@ -20,25 +16,17 @@ function HomePage() {
     };
   }, []);
 
-  // Define a function for handling the logout button click event
-  const handleLogout = async () => {
-    await logout(); // Call the logout function to log the user out of their Google account
-  };
-
-  // Render the component's UI
   return (
     <div className={styles.container}>
       <h1>Welcome to the Coppelia Dashboard!</h1>
       <div className={styles.buttons}>
-        {/* Add a navigation link to the IntervalController page */}
         <Link href="http://localhost:3000/IntervalController">
           <button className={styles.button}>Dashboard</button>
         </Link>
-        {/* Conditionally render a Logout button or a Login button based on the user state */}
         {user ? (
-          <button className={styles.button} onClick={handleLogout}>
-            Logout
-          </button>
+          <Link href="/LogoutGoogle">
+            <button className={styles.button}>Logout</button>
+          </Link>
         ) : (
           <Link href="/LoginGoogle">
             <button className={styles.button}>Login</button>
@@ -49,5 +37,4 @@ function HomePage() {
   );
 }
 
-// Export the HomePage component as the default export of this module
 export default HomePage;
